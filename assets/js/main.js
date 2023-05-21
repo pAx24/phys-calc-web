f75a3a660e42c1c95dfb5ab8f74514b77cb46251
pAx24 <84711602+pAx24@users.noreply.github.com>
2023-05-21 21:41:31 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
+window.onload = function() {
+    if(navigator.userAgentData.mobile) {
+        const mobileBlock = document.createElement("div");
+        mobileBlock.style = "width: 100%; height: 100%; background-color: white; position: absolute;";
+        
+        const mobileText = document.createElement("p");
+        mobileText.style = "text-size: 0.5cm";
+    }
+}
+
 function SwapFrame(iframe) {
     switch(iframe) {
         case 1: document.getElementById("iframe").src = "./formulas.html"; break;
